d004cd1596e6cbb2e2d62f1310b87a9b9098a3a8
HeannaReis <heannareis@gmail.com>
2022-10-23 21:35:20 -0300

    Update Maps

--- appLocPet/src/pages/Home/index.js
+++ appLocPet/src/pages/Home/index.js
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker}from 'react-native-maps';
 
 import * as Location from 'expo-location';
 
 const App = () => {
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
     (async () => {
       
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+        console.log('Permission to access location was denied');
         return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      console.log(location);
     })();
   }, []);
   return (
     <View style={styles.container}>
       <Text>Maps</Text>
       <MapView style={styles.map}
-      initialRegion={{
+       initialRegion={{
        latitude: -23.62348364204039,
        longitude: -46.74943498729773,
        latitudeDelta: 0.0122,
        longitudeDelta: 0.0021
       }}      
+      
       />
     </View>
   );
@@ -39,8 +40,8 @@ const App = () => {
 const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    marginTop: 10,
-    marginStart: 25,
+    width: '100%',
+    height: '100%',
   },
   map: {
     flex: 1
