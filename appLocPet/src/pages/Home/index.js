4436b72c0515a0ee400317a3ddc5a23bdb882a38
HeannaReis <heannareis@gmail.com>
2022-10-29 21:15:59 -0300

    Add TabNavegation

--- appLocPet/src/pages/Home/index.js
+++ appLocPet/src/pages/Home/index.js
@@ -1,54 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native-animatable";
 
-import MapView, { Marker} from 'react-native-maps';
-
-import * as Location from 'expo-location';
-
-const App = () => {
-  const [location, setLocation] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      console.log(location);
-    })();
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>Maps</Text>
-      <MapView style={styles.map}
-       initialRegion={{
-       latitude: -23.62348364204039,
-       longitude: -46.74943498729773,
-       latitudeDelta: 0.0122,
-       longitudeDelta: 0.0021
-      }}      
-      showUserLocation
-      mapaType="terrain"
-      >
-       
-        </MapView>
+export default function Home (){
+  return(
+    <View>
+      <Text> 
+        Bem Vindo a nossa Home
+      </Text>
     </View>
-  );
-};
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  map: {
-    flex: 1
-  },
-});
-
-export default App;
+  )
+}
--- appLocPet/src/routes/index.js
+++ appLocPet/src/routes/index.js
@@ -1,35 +1,58 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Welcome from '../pages/Welcome'
 import SignIn from '../pages/SignIn'
 import Register from '../pages/Register'
 import Home from '../pages/Home'
+import Maps from '../pages/Maps'
+
+const Tab = createBottomTabNavigator()
+
+function MyTabs() {
+    return (
+        <Tab.Navigator>
+
+            <Tab.Screen name="Home" component={Home} />
+
+            <Tab.Screen name="Login" component={SignIn} />
+
+            <Tab.Screen name="Cadastro" component={Register} />
+
+            <Tab.Screen name="Mapas" component={Maps} />
+
+        </Tab.Navigator>
+    )
+}
 
 const Stack = createNativeStackNavigator()
 
-export default function Routes(){
-    return(
+export default function Routes() {
+  return (
     <Stack.Navigator>
-        <Stack.Screen 
-        name='Welcome'
+      <Stack.Screen
+        name="Welcome"
         component={Welcome}
         options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-        name='SignIn'
+      />
+
+      <Stack.Screen
+        name="SignIn"
         component={SignIn}
         options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-        name='Register'
+      />
+
+      <Stack.Screen
+        name="Register"
         component={Register}
         options={{ headerShown: false }}
-        />
-        <Stack.Screen
+      />
+
+      <Stack.Screen
         name="Home"
-        component={Home}
+        component={MyTabs}
         options={{ headerShown: false }}
-        />
-    </Stack.Navigator>        
-)
+      />
+    </Stack.Navigator>
+  )
 }
